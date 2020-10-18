100075d571120113931df2551f5e71ff8a4e943a
Marta <marta.nunes.abreu@gmail.com>
2020-10-18 15:33:50 +0100

    connected routes and templates

--- venv/app.py
+++ venv/app.py
@@ -1,12 +1,25 @@
-# File describing the web server side management
+
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+from flask import request
+from flask import g, url_for
 import sqlite3 as sql
-from flask import Flask, g, render_template, request
 from db_handler import *
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/delete_booking", methods=["POST"])
+def delete_booking():
+    a = request.form.get("timeslot")
+    b = request.form.get("author")
+    # my_db.delete_timeslot(a)
+    return render_template("plain.html")
 
 @app.route('/bookings')
 def view_bookings():
@@ -19,4 +32,9 @@ def view_users():
     my_db = DBHandler()
     users = my_db.get_users()
     return render_template("users_page.html", rows = users)
+
+
+@app.route('/edit')
+def edit_booking():
+    return render_template("delete.html")
 
